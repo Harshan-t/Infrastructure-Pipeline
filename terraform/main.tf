f53572beabaf99ce7566a2c6e7637033bcb22521
Harshan-t <harshanthangavel123@gmail.com>
2026-01-01 20:47:08 +0530

    Updated subnet and vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 module "eks" {
@@ -9,8 +9,8 @@ module "eks" {
   cluster_name    = "demo-eks"
   cluster_version = "1.29"
 
-  subnet_ids = ["subnet-xxxx", "subnet-yyyy"]
-  vpc_id     = "vpc-xxxx"
+  subnet_ids = ["subnet-056ffe4638c24d273", "subnet-0909f92c152869571"]
+  vpc_id     = "vpc-062705c97c37cc843"
 
   eks_managed_node_groups = {
     default = {
@@ -18,7 +18,7 @@ module "eks" {
       max_size     = 2
       min_size     = 1
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t2.micro"]
     }
   }
 }
